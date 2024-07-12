b8f5435523bee3dd0185a6071fee980bd9a9c7f1
mattbixley <mattbixley72@gmail.com>
2024-07-12 15:01:26 +1200

    basic geospatial

---
+++ scripts/geospatial.r
@@ -0,0 +1,16 @@
+# geospatial work with terra
+library(terra)
+
+# Create a SpatRaster 'a' with values
+a <- rast(ncols = 40, nrows = 40, xmin = -110, xmax = -90, ymin = 40, ymax = 60, crs = "+proj=longlat +datum=WGS84")
+values(a) <- 1:ncell(a)
+
+# Define a new CRS for 'b'
+newcrs <- "+proj=lcc +lat_1=48 +lat_2=33 +lon_0=-100 +datum=WGS84"
+
+# Create a new SpatRaster 'b' by projecting 'a'
+b <- rast(ncols = 94, nrows = 124, xmin = -944881, xmax = 935118, ymin = 4664377, ymax = 7144377, crs = newcrs)
+w <- project(a, b)
+
+plot(w)
+

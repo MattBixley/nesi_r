80cf72517ea7cdbe3c25b2cff0d02c85fa6fc561
Matt Bixley <mattbixley72@gmail.com>
2024-08-15 14:56:16 +1200

    Update parallel.r

--- scripts/parallel.r
+++ scripts/parallel.r
@@ -1,28 +1,56 @@
+### Packages
 library(doParallel)
-library(furrr)
+library(tidyverse)
+library(tidymodels)
+library(palmerpenguins)
+library(ranger)
 
-# Set up a parallel backend with the number of workers (cores) you want to use
-num_cores <- 2  # Adjust the number of cores as needed
-cl <- makeCluster(num_cores)
-registerDoParallel(cl)
+### Data for modelling
+penguins_df <- penguins %>% drop_na() %>% select(-year)
 
-# Set up a parallel plan with furrr
-plan(cluster, workers = cl)
+### Split into train and test
+penguin_split <- initial_split(penguins_df, strata = species)
+penguin_train <- training(penguin_split)
+penguin_test <- testing(penguin_split)
 
-# Example function to apply
-example_function <- function(x) {
-  Sys.sleep(2)  # Simulate a time-consuming task
-  x * 2
+### Define Random Forest Model
+rf_spec <- rand_forest(trees = 1000,
+                       mtry = tune(),
+                       min_n = tune()) %>% 
+  set_mode("classification") %>% 
+  set_engine("ranger")
+
+### Cross Validation Sets to tune RF
+penguin_cv <- vfold_cv(penguins_df, strata = species)
+
+### Add formula and model together with workflow
+tune_wf <- workflow() %>% 
+  add_formula(species ~ .) %>% 
+  add_model(rf_spec)
+
+### Parallel Function for Model Tuning
+parallel_rf <- function(cores = 2, grid = 20){
+  numCores <- cores
+  cl <- makeCluster(numCores)
+  registerDoParallel(cl)
+ 
+  ### Tune HyperParameters
+  start_time <- Sys.time()
+  rf_tune <- tune_grid(
+    tune_wf,
+    resamples = penguin_cv,
+    control = control_resamples(save_pred = TRUE),
+    grid = grid
+  )
+  rf_tune
+  end_time <- Sys.time()
+   
+  ### Stop Cluster
+  stopCluster(cl)
+  
+  return(paste0("Tuning time with ", numCores, " cores is ", round(end_time - start_time, 2), " seconds"))
+  
 }
 
-# List of numbers to process
-numbers <- list(1, 2, 3, 4, 5)
-
-# Apply the function in parallel using future_map()
-result <- future_map(numbers, example_function)
-
-print(result)
-
-# Stop the cluster
-stopCluster(cl)
-
+# 1 core, grid = 8
+parallel_rf(1,8)

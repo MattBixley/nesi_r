613f11c47f2d42a5c01945ad2433689e3fc17a8d
Matt Bixley <mattbixley72@gmail.com>
2024-08-21 09:30:41 +1200

    Update parallel.r

--- scripts/parallel.r
+++ scripts/parallel.r
@@ -1,6 +1,6 @@
 ### tidymodels bootstramp example to test that multiple cores
-library(tidyverse)
 library(tidymodels)
+library(tidyverse)
 library(palmerpenguins)
 library(doParallel)
 # devtools::install_github("G-Thomson/Manu")
@@ -45,7 +45,7 @@ penguin_wf <- workflow() %>%
 cl <- makeCluster(cores)
 registerDoParallel(cl)
 
-# repeat each test 11 times, drop 1st iteration
+# repeat each test 10 times (actuall 11 and then drop the 1st)
 time <- numeric()
 for(i in 1:11){
  
@@ -71,25 +71,27 @@ for(i in 1:11){
   time[i] <- end - start
 
 }
-return(time)
 stopCluster(cl)
+return(time)
 }
+
 
 ### run the fit with different cpus
 count <- 1
-cpus_min <- 1
-cpus_max <- 8
-steps <- 4
+cpus_min <- 4
+cpus_max <- 90
+steps <- 8
 cores_result <- data.frame(matrix(NA, nrow = steps, ncol = 4))
 names(cores_result) <- c("cores", "time_mean", "time_var", "time_sd" )
 
 for(j in floor(seq(cpus_min, cpus_max, length.out = steps))){
-  time <- cores_test(cores = j, boot = 100)
+  time <- cores_test(cores = j, boot = 2000)
   cores_result[count,1] <- j
   cores_result[count,2] <- round(mean(time[-1]),3)
   cores_result[count,3] <- round(var(time[-1]),3)
   cores_result[count,4] <- round(sd(time[-1]),3)
   count <- count + 1
+  print(time)
 }
 
 cores_result
@@ -101,7 +103,7 @@ ggplot(cores_result, aes(x = cores, y = time_mean)) +
   geom_line(col = selected_colours[1], linewidth =1.1) +
   geom_point(col = selected_colours[2], size = 3, alpha = 0.8) +
   labs(x = "Number of cores", y = "Time in seconds") +
-  ggtitle("Boostrapping Cores vs Run Time\nmean of 10 replicates") +
+  ggtitle("Boostrapping Cores vs Run Time\nmean of 10 reps of 2000 resamples") +
   theme_minimal()
 
 #glm_rs
@@ -124,4 +126,5 @@ ggplot(cores_result, aes(x = cores, y = time_mean)) +
 
 #penguin_final$.workflow[[1]] %>%
 #  tidy(exponentiate = TRUE)
+
 
